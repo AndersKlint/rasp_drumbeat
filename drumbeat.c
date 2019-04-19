79f6e68d363b79c388190e48ce03ebf109622447
Anders Klint <anders.klint519@gmail.com>
2019-04-19 13:49:53 +0200

    interrupt and multi channel

--- drumbeat.c
+++ drumbeat.c
@@ -1,12 +1,64 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 #include <stdio.h>
+#include <alsa/asoundlib.h>
 
 
 #define button_pin 18 // pin 12
+#define button2_pin 23 // pin 16
+
+static char *device = "default";                        /* playback device */
+snd_output_t *output = NULL;
+
+int play_sound(unsigned char buffer[], size_t buffer_size, snd_pcm_t *handle)
+{
+		unsigned int i;
+		snd_pcm_sframes_t frames;
+        for (i = 0; i < 16; i++) {
+                frames = snd_pcm_writei(handle, buffer, buffer_size);
+                if (frames < 0)
+                        frames = snd_pcm_recover(handle, frames, 0);
+                if (frames < 0) {
+                        printf("snd_pcm_writei failed: %s\n", snd_strerror(frames));
+                        break;
+                }
+                if (frames > 0 && frames < (long)buffer_size)
+                        printf("Short write (expected %li, wrote %li)\n", (long)buffer_size, frames);
+        }
+       // snd_pcm_close(handle);
+        return 0;
+}
+
+void init_sound_engine(snd_pcm_t *handle) 
+{
+	    int err;
+        if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+                printf("Playback open error: %s\n", snd_strerror(err));
+                exit(EXIT_FAILURE);
+        }
+        if ((err = snd_pcm_set_params(handle,
+                                      SND_PCM_FORMAT_U8,
+                                      SND_PCM_ACCESS_RW_INTERLEAVED,
+                                      1,
+                                      48000,
+                                      1,
+                                      500000)) < 0) {   /* 0.5sec */
+                printf("Playback open error: %s\n", snd_strerror(err));
+                exit(EXIT_FAILURE);
+        }
+}
 
 int main(void) 
 {
+	/*unsigned char buffer[16*1024];                     
+	unsigned int i;
+	snd_pcm_t *handle;
+	init_sound_engine(handle);
+    for (i = 0; i < sizeof(buffer); i++)
+        buffer[i] = random() & 0xff;
+        
+    play_sound(buffer, sizeof(buffer), handle);
+	*/
 	printf("test start\n");
 	//system("omxplayer /home/pi/Downloads/samples/808_kick.wav");
 	
@@ -16,12 +68,27 @@ int main(void)
 	pinMode(button_pin, INPUT);
 	pullUpDnControl(button_pin, PUD_UP);
 	
+	pinMode(button2_pin, INPUT);
+	pullUpDnControl(button2_pin, PUD_UP);
+	
+	int prev_b1_state = 1;
+	int prev_b2_state = 1;
 		
 	while(1){
-		if(digitalRead(button_pin) == 0){
-			system("aplay /home/pi/Downloads/samples/808_kick.wav");
-			delay(10);
+		if(digitalRead(button_pin) == 0 && prev_b1_state == 1){
+			system("aplay -d 1 -c 32 /home/pi/Downloads/samples/kick.wav &");
+			prev_b1_state = 0;
 		}
+		else if (digitalRead(button_pin) == 1)
+			prev_b1_state = 1;
+			
+		if(digitalRead(button2_pin) == 0 && prev_b2_state == 1){
+			system("aplay -d 1 -c 32 /home/pi/Downloads/samples/snare.wav &");
+			prev_b2_state = 0;
+		}
+		else if (digitalRead(button2_pin) == 1)
+			prev_b2_state = 1;
+
 		delay(1);
 	}
 		
